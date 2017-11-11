9101fb558de2756f4a913f8dbeb23fed362ffa2d
xmc1993 <xumaocheng@baidu.com>
2017-11-11 15:48:59 +0800

    fix

--- src/main/java/com/cncf/entity/User.java
+++ src/main/java/com/cncf/entity/User.java
@@ -14,8 +14,6 @@ public class User implements Serializable{
     private String mobile;//手机号
     private String password;//密码
     private String company;//公司
-    private String province;//省份
-    private String city;//城市
     private String detailAddress;
     private String email;//邮箱
     private String qq;//qq
